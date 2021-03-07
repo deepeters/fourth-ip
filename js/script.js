0430ba26f01eeb7a7619f3fc6a71657e0c8b46d1
Dennis Peters <dennis95peters@gmail.com>
2021-03-07 23:02:32 +0300

    This commit builds the order modal content

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-let modalBtn = document.getElementById("modal-btn")
-let modal = document.querySelector(".modal")
-let closeBtn = document.querySelector(".close-btn")
-modalBtn.onclick = function(){
-  modal.style.display = "block"
-}
-closeBtn.onclick = function(){
-  modal.style.display = "none"
-}
-window.onclick = function(e){
-  if(e.target == modal){
+$(document).ready(function() {
+    let modalBtn = document.getElementById("modal-btn")
+    let modal = document.querySelector(".modal")
+    let closeBtn = document.querySelector(".close-btn")
+    modalBtn.onclick = function(){
+    modal.style.display = "block"
+    }
+    closeBtn.onclick = function(){
     modal.style.display = "none"
-  }
-}
+    }
+    window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display = "none"
+    }
+    }
+})
+
+$(document).ready(function() {
+    $("form#contactForm").submit(function(event) {
+        event.preventDefault();
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+
+
+        if (name && email) {
+            alert(name + ", your message has been received. We will process it and provide feedback.");
+        } else {
+            alert("Please enter your name and email!");
+        }
+    })
+
+
+})
+
